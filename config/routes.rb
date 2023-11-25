87b7c906da52622f757ed9f5e62cfb9aff42b7da
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-11-25 15:05:21 -0700

    Setup routes using namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :customers do
+        resources :subscriptions, only: [:create, :index, :update]
+      end
+    end
+  end
 end
