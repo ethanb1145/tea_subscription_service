0bbdee3916bc77c62510853d612896149bd3e136
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-11-25 15:07:27 -0700

    Adjust factory

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :subscription do
     title { Faker::Lorem.sentence }
     price { Faker::Commerce.price(range: 5.0..20.0) }
-    status { ['active', 'cancelled'].sample }
+    status { ['subscribed', 'cancelled'].sample }
     frequency { ['daily', 'weekly', 'monthly'].sample }
     association :customer
     association :tea
